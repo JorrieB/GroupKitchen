7eda7663a0c2adb5439aa82828ccfd31e8f267f6
Laura Breiman <lauracle@mit.edu>
2015-04-09 19:39:56 -0400

    scheduling modal

--- homePage.js
+++ homePage.js
@@ -1,6 +1,18 @@
+var foodItems = ["Salad", "Side Dish", "Main Course", "Dessert"];
+
 $(document).ready( function(){
     $('#graphTabs a').click(function (e) {
       e.preventDefault()
       $(this).tab('show')
     });
+        
+    function setupTable(){
+        $(".scheduling-table").append("<thead><tr><th>Quantity</th><th>Food Item</th></thead><tbody class ='scheduling-body'></tbody>");
+        
+        for(var foodItem in foodItems){
+            $(".scheduling-body").append("<tr><td class='quantity_cell'><input type='text' class='form-control quantity-input' id='foodQuantity'></td><td>" + foodItems[foodItem] + "</td></tr>");
+        }
+    }
+    
+    setupTable();
 });
