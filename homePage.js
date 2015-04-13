b8b782fdc2117cce976dfdabb4cb7817832bb48b
BrendanSChang <BrendanSChang@gmail.com>
2015-04-12 21:04:22 -0400

    fixed the skeleton of save event and added a TODO comment

--- homePage.js
+++ homePage.js
@@ -42,8 +42,22 @@ $(document).ready( function(){
 		*/
 	});
 	
-	// should add the event to the calendar, but I haven't had time to test it
-	$('#save-event').('click', function() {
+	// TODO: fix adding of events
+	// There needs to be some logic built into this to figure out the correct date
+	// to add the pick-ups. This shouldn't be too hard, as it's simply choosing the
+	// next weekday from the current date. If no weekday is selected, then a pick-up
+	// should be added for the current day. The repeat weekly functionality might be
+	// harder to add in, but we can not include it if need be for GR4.
+	//
+	// The sequence for adding an event is roughly as follows:
+	//   - click the schedule button
+	//   - fill in the necessary information in the modal
+	//   - convert that information into an event using the renderEvent function for fullCalendar
+	//   - close the modal
+	//
+	// Once that basic functionality is in place, it would be best to add in some basic
+	// error checking/validation.
+	$('#save-event').click(function() {
 		var start = 'start time';
 		var end = 'end time';
 		var title = 'title';
