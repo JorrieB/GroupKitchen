897268183feafa39951db248413c47c6836e7c80
Laura Breiman <lauracle@mit.edu>
2015-05-04 13:44:28 -0400

    autofilled day

--- calendar.js
+++ calendar.js
@@ -2,10 +2,23 @@ $(document).ready(function() {
     
     function addPickup(){
         var dayOfWeek = $("#days").children().eq($(this).index()).html();
+        var firstThreeLetters = dayOfWeek.substring(0,3);
+        var dayButton = "button"+firstThreeLetters;
         $('#scheduleModal').modal('show');
+        $("#"+dayButton).parent().addClass("active");
+        $("#"+dayButton).prop("checked","checked");
     }
     
     $(".day").on("click", addPickup);
     $(".pickup").on("click", addPickup);
+    
+    $('#scheduleModal').on('hidden.bs.modal', function () {
+        var daysOfWeek = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
+        for(var threeDay in daysOfWeek){
+            $("#button"+daysOfWeek[threeDay]).parent().removeClass("active");
+            $("#button"+daysOfWeek[threeDay]).prop("checked","unchecked");    
+        }
+    })
+    
     
 });
