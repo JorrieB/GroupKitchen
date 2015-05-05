776a0ee6e0c2f3b3d36977040f6d0b9bf70ddb6c
Laura Breiman <lauracle@mit.edu>
2015-05-05 18:45:50 -0400

    pre complete is back

--- calendar.js
+++ calendar.js
@@ -2,8 +2,9 @@ function addPickup(){
     var dayOfWeek = $("#days").children().eq($(this).index()).html();
     var firstThreeLetters = dayOfWeek.substring(0,3);
     var dayButton = "button"+firstThreeLetters;
+    console.log(dayButton);
     $('#scheduleModal').modal('show');
-    $("#"+dayButton).parent().addClass("active");
+    $("#"+dayButton).addClass("active");
     $("#"+dayButton).prop("checked","checked");
     $("#pickup-start").val("6:00");
     $("#pickup-end").val("7:00");
@@ -18,7 +19,7 @@ $(document).ready(function() {
     $('#scheduleModal').on('hidden.bs.modal', function () {
         var daysOfWeek = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
         for(var threeDay in daysOfWeek){
-            $("#button"+daysOfWeek[threeDay]).parent().removeClass("active");
+            $("#button"+daysOfWeek[threeDay]).removeClass("active");
             $("#button"+daysOfWeek[threeDay]).prop("checked","unchecked");    
         }
     })
