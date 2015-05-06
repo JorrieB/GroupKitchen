0f78835a217aebc3a7eb5d65bf6116120c3027e4
Laura Breiman <lauracle@mit.edu>
2015-05-06 12:53:19 -0400

    stuff

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function fillFuturePickups(){
             var foodForPickup = $(foodsForPickup[index]).html();
 
             if(foodForPickup){
-				var newListElement = "<li>On: "+dayFormatted+", "+foodForPickup+"</li>";
+				var newListElement = "<li>On: "+dayFormatted+", "+foodForPickup.split("<")[0]+"<a class = 'deleteButton delete"+index+"' data-pickupNum="+index+" href='#'><i class='fa fa-times'></i></a></li>";
             $('.futureList').append(newListElement);
             }
         }
